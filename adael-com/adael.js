28382ad938610c58b700fc1625da489252c40f4d
goingobsolete <goingobsolete@gmail.com>
2024-06-10 11:13:30 +0200

    added sliders for R G and B

---
+++ adael-com/adael.js
@@ -0,0 +1,13 @@
+let brushSizeX = float 10;
+let brushSizeY = float 10;
+let mfont =  Helvetica;
+let textString = "Adael Hernandez";
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+}
+
+function draw() {
+    
+}
